3e29d9d6daff6829ed2ea99d380d8100e704da23
David Karapetyan <david.karapetyan@gmail.com>
2013-09-01 11:23:38 -0400

    converted to c++

--- ch1/rm_dup_string.cpp
+++ ch1/rm_dup_string.cpp
@@ -1,43 +1,44 @@
 //remove duplicate characters in a string
-//
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <utility>
+using std::string;
+using std::cin;
+using std::cout;
+using std::getline;
+using std::pair;
 
-#define SIZE 100
 
-int da_array[SIZE]={0};
-
-
-void rm_dup(char *string, int len );
+void rm_dup(string &my_string);
 
 
 
 int main()
 {
-    char *string = NULL;
-    size_t linecap = 0;
-
-    puts("Please enter a string");
-
-    int len = getline(&string, &linecap, stdin);
-    rm_dup(string, len);
-    puts(string);
+    string my_string;
+    cout << "Please enter a my_string \n";
+    getline(cin, my_string);
+    rm_dup(my_string);
+    cout << my_string;
     return 0;
 }
 
 
-void rm_dup(char *string, int len)
+void rm_dup(string &my_string)
 {
-    int last = 0;
-    for (int i = 0; i < len; i++)
+    std::unordered_multimap<char, size_t> my_table;
+    size_t last = 0; 
+    size_t len = my_string.size();
+
+    for (size_t i = 0; i < len; i++)
     {
-        da_array[(int)string[i]] += 1; 
-        if (da_array[(int)string[i]] == 1)
+        my_table.insert(std::pair<char, size_t>(my_string[i],0));
+        if (my_table.count(my_string[i]) == 1)
         {
-            string[last] = string[i];
-            last++;
+            my_string[last++] = my_string[i];
         }
     }
-    string[last] = '\0';
+    my_string.erase(last, len -1);
 } 
