8a1af03a4d4db4387e9e4f2a4da66bbe1ab1873e
David Karapetyan <david.karapetyan@gmail.com>
2013-09-01 01:55:27 -0400

    c++ rotate image 90 degrees clockwise

--- ch1/rotate_image.cpp
+++ ch1/rotate_image.cpp
@@ -1,102 +1,72 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
+// rotate square picture 90 degrees clockwise
 
-#define N 5
+#include <iostream>
+#include <ctime>
+#include <vector>
+#include <utility>
+const int N=5;
 
+template <class T>
+using matrix = std::vector<std::vector <T> >; 
 
-typedef struct pixel
-{
-    int some_data;
-} pixel;
-
-
-
-void swap(pixel **array, int i, int j, int l, int m);
-void rotate_picture_90(pixel **array, size_t len);
-void rotate_rec_90(pixel **array, int i, int j, int len);
+void rotate_picture(matrix<int> &my_matrix);
+void rotate_rec(matrix<int> &my_matrix, int i, int j, int len);
 
 
 int main()
 {
-    srand(time(NULL));
+    srand(time(nullptr));
 
-    pixel **array = malloc(sizeof(pixel *) * N);
-    // assign memory for pointers in matrix
-    for (int j = 0; j < N; j++)
-        array[j] = malloc(sizeof(pixel) * N);
+    matrix<int> my_matrix(N, std::vector<int> (N));
 
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
-            array[i][j].some_data = rand()%10; 
+            my_matrix[i][j] = rand()%10; 
     }
 
-    // initialize matrix
+    // print matrix
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
-            printf("%d ", array[i][j].some_data);
+            std::cout <<  my_matrix[i][j] << " ";
             if (j == N-1)
-                printf("\n");
+                std::cout << "\n";
         }
     }
 
-    /*rotate_rec_90(array, 0, 0, N);*/
-    rotate_picture_90(array, N);
-    /*swap(array, 0, 1, 1, 0);*/
-
-    printf("\n");
+    rotate_picture(my_matrix);
+    std::cout << "\n";
 
     // print rotated matrix
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
-            printf("%d ", array[i][j].some_data);
+            std::cout << my_matrix[i][j] << " ";
             if (j == N-1)
-                printf("\n");
+                std::cout << "\n";
         }
     }
-
-    // free memory in memory (stored on the heap)
-
-    for (int i = 0; i < N; i++)
-    {
-        free(array[i]);
-    }
-
-    free(array);
-
-
     return 0;
 }
 
-
-void rotate_picture_90(pixel **array, size_t len)
+void rotate_picture(matrix<int> &my_matrix)
 {
     int i = 0;
     int k = 0;
-    while((int)len > 0)
+    int len = my_matrix[0].size();
+    while(len > 0)
     {
-        rotate_rec_90(array, i+k, i+k, len);
+        rotate_rec(my_matrix, i+k, i+k, len);
         len-=2;
         k++;
     }
-
-
-
 }
 
-void swap(pixel **array, int i, int j, int l, int m)
-{
-    pixel temp = array[i][j];
-    array[i][j] = array[l][m];
-    array[l][m] = temp;
-}
 
-void rotate_rec_90(pixel **array, int i, int j, int len)
+void rotate_rec(matrix<int> &my_matrix, int i, int j, int len)
 {
     int tlc_row = i;
     int tlc_col = j;
@@ -108,18 +78,17 @@ void rotate_rec_90(pixel **array, int i, int j, int len)
     int brc_col = j + len - 1;
 
     for (int k=1; k < len-1; k++)
-        swap(array, tlc_row, tlc_col + k,  trc_row + k, trc_col); // swap top of square with right side, mod corners
+        std::swap<int>(my_matrix[tlc_row][tlc_col + k], my_matrix[trc_row + k][trc_col]); // swap top of square with right side, mod corners
 
     for (int k=1; k < len-1; k++)
-        swap(array, tlc_row, tlc_col + k, brc_row, brc_col - k); // inverted swap top of square with bottom, mod corners
+        std::swap<int>(my_matrix[tlc_row][tlc_col + k], my_matrix[brc_row][brc_col - k]); // inverted swap top of square with bottom, mod corners
 
     for (int k=1; k < len-1; k++)
-        swap(array, tlc_row, tlc_col + k, blc_row - k, blc_col); // inverted swap top of square with left side, mod corners 
+        std::swap<int>(my_matrix[tlc_row][tlc_col + k], my_matrix[blc_row - k][blc_col]); // inverted swap top of square with left side, mod corners 
 
     // rotate corners 90 degrees
-    swap(array, tlc_row, tlc_col, trc_row, trc_col); // swap top left corner with top right 
-    swap(array, tlc_row, tlc_col, brc_row, brc_col); // swap top left with bottom right
-    swap(array, tlc_row, tlc_col, blc_row, blc_col); // swap top left with bottlm left
-
+    std::swap<int>(my_matrix[ tlc_row][tlc_col], my_matrix[ trc_row][ trc_col]); // swap top left corner with top right 
+    std::swap<int>(my_matrix[ tlc_row][tlc_col], my_matrix[brc_row][brc_col]); // swap top left with bottom right
+    std::swap<int>(my_matrix[tlc_row][tlc_col], my_matrix[blc_row][blc_col]); // swap top left with bottlm left
 }
 
