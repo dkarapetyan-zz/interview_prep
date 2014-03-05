12e816103113fb6fa4b395f7bbb44a4dd1eaf8cd
David Karapetyan <david.karapetyan@gmail.com>
2014-03-05 12:16:48 -0500

    moved many files for appcode

---
+++ ch1/rotate_image/rotate_image/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  rotate_image
+//
+//  Created by David Karapetyan on 3/5/14.
+//  Copyright (c) 2014 david. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+
+    return 0;
+}
